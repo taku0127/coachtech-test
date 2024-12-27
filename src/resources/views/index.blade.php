743f8e718db803b6c2ca74dd0e83943383d9c283
taku_y <y.taku84@gmail.com>
2024-12-27 14:19:55 +0900

    デフォルトページ作成、scssを使えるように設定、reset.css追加

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,3 @@
+@extends('layouts.default')
+@section('title','お問い合わせフォーム入力ページ')
+@section('h1','Contact')
---
+++ src/resources/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+</head>
+<body>
+<header class="c-header">
+    <div class="c-header_title">FashionablyLate</div>
+</header>
+<main>
+    <div class="c-title">
+        <h1 class="c-title_txt">
+            @yield('h1')
+        </h1>
+    </div>
+    @yield('content')
+</main>
+</body>
+</html>
